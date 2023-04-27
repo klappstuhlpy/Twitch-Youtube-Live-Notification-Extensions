4246baa64f66bc3a31bc031c0ad9b027f2801563
Klappstuhl <benedikt.schredl@gmx.de>
2023-04-27 15:53:37 +0200

    Update youtube.py
    moved description to README

--- youtube.py
+++ youtube.py
@@ -51,26 +51,6 @@ class YouTubeRequestError(Exception):
 
 class config(abc.ABCMeta):
     """A class for getting and setting the config.json file.
-
-    Example JSON File
-    -----------------
-    {
-        "youtube": {
-            "api_key": "",
-            "channel_id": 0,
-            "watchlist": [
-                "PewDiePie"
-            ]
-        }
-    }
-
-    Example Folder Hierarchy
-    ------------------------
-    YourBotFolder <- Second "parent" folder
-        - cogs  <- First "parent" folder
-            - youtube.py  <- Current File
-        - main.py
-        - config.json
     """
 
     path = Path(__file__).parent.parent / "config.json"
