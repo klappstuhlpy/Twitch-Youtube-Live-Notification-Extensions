57534464cd68aefd539a360fb84065cf47f7db9e
Klappstuhl <benedikt.schredl@gmx.de>
2023-04-27 15:51:59 +0200

    Add files via upload

---
+++ twitch.py
@@ -0,0 +1,279 @@
+# -*- coding: utf-8 -*-
+
+"""
+MIT License
+
+Copyright (c) 2022-Present Klappstuhl
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+import abc
+import logging
+import random
+import time
+from pathlib import Path
+from typing import List, Dict, Any, Optional, NamedTuple
+
+import aiohttp
+import discord
+import json
+
+from discord.ext import commands, tasks
+import datetime
+
+from discord.utils import cached_slot_property
+
+logger = logging.getLogger(__name__)
+
+
+class TwitchRequestError(Exception):
+    def __init__(self, reason: str, status: int):
+        self.status = status
+        self.reason = reason
+        super().__init__(f"Request returned {status} thrown at: {reason}")
+
+
+GRANT_URL = "https://id.twitch.tv/oauth2/token"
+TWITCH_ICON_URL = "https://media.discordapp.net/attachments/1062074624935993427/1101142491450835036/5968819.png"
+
+
+class config(abc.ABCMeta):
+    """A class for getting and setting the config.json file.
+
+    Example JSON File
+    -----------------
+    {
+        "twitch": {
+            "client_id": "",
+            "client_secret": "",
+            "channel_id": 0,
+            "watchlist": [
+                "montanablack88"
+            ],
+            "expiry": 0,
+            "bearer_token": ""
+        }
+    }
+
+    Example Folder Hierarchy
+    ------------------------
+    YourBotFolder <- Second "parent" folder
+        - cogs  <- First "parent" folder
+            - twitch.py  <- Current File
+        - main.py
+        - config.json
+    """
+
+    path = Path(__file__).parent.parent / "config.json"
+
+    @classmethod
+    def set(cls, **params: dict | Any) -> None:
+        payload = cls.get()
+
+        with open(cls.path, "w") as file:
+            payload["twitch"].update(params)
+            json.dump(payload, file, indent=4)
+
+    @classmethod
+    def get(cls) -> Dict[str, Any]:
+        with open(cls.path, 'r', encoding='utf-8') as f:
+            return json.load(f)
+
+
+class TwitchUser(NamedTuple):
+    id: str
+    login: str
+    display_name: str
+    type: str
+    broadcaster_type: str
+    description: str
+    profile_image_url: str
+    offline_image_url: str
+    view_count: int
+
+    @property
+    def url(self) -> str:
+        return f"https://twitch.tv/{self.login}"
+
+
+class TwitchStream(NamedTuple):
+    id: str
+    user: TwitchUser
+    game_id: str
+    game_name: str
+    type: str
+    title: str
+    tags: List[str]
+    viewer_count: int
+    started_at: str
+    language: str
+    thumbnail_url: str
+
+
+class TwitchNotifications(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot: commands.Bot = bot
+        self.session: aiohttp.ClientSession = aiohttp.ClientSession()
+
+        self.online_users: List[str] = []
+
+    async def cog_load(self) -> None:
+        self.refresh_notify_check.start()
+
+    async def cog_unload(self) -> None:
+        if not self.session.closed:
+            await self.session.close()
+        self.refresh_notify_check.cancel()
+
+    def _expiry(self, expiry: Optional[float] = None) -> float:
+        if expiry:  # Set expiry time
+            config.set(expiry=expiry)
+        return config.get()["twitch"].get("expiry", None)
+
+    def _bearer_token(self, bearer_token: Optional[str] = None) -> str:
+        if not self._expiry() or (self._expiry() < time.time()):
+            logger.debug("Refreshing bearer token")
+            self.bot.loop.create_task(self._get_bearer_token())
+
+        if bearer_token:
+            config.set(bearer_token=bearer_token)
+        return config.get()["twitch"].get("bearer_token", None)
+
+    @cached_slot_property(name="_cs_grant_params")
+    def grant_params(self) -> dict:
+        return {'client_id': config.get()["twitch"]["client_id"],
+                'client_secret': config.get()["twitch"]["client_secret"],
+                'grant_type': 'client_credentials',
+                'Content-Type': 'application/x-www-form-urlencoded'}
+
+    @cached_slot_property(name="_cs_bearer_headers")
+    def bearer_headers(self) -> dict:
+        return {'Authorization': f'Bearer {self._bearer_token()}',
+                'Client-Id': config.get()["twitch"]["client_id"]}
+
+    async def _get_bearer_token(self) -> None:
+        async with self.session.post(GRANT_URL, params=self.grant_params) as resp:
+            if resp.status != 200:
+                raise TwitchRequestError(resp.reason, resp.status)
+
+            data = await resp.json()
+            self._expiry(expiry=(time.time() + (int(data['expires_in']) - 10)))
+            self._bearer_token(bearer_token=data['access_token'])
+
+    async def get_users(self, login_names: List[str]) -> List[TwitchUser]:
+        payload = {"login": login_names}
+
+        async with self.session.get("https://api.twitch.tv/helix/users", params=payload,
+                                    headers=self.bearer_headers) as resp:
+            if resp.status != 200:
+                raise TwitchRequestError("Could not get user IDs. Maybe refresh the bearer token?", resp.status)
+
+            data = await resp.json()
+            return [TwitchUser(
+                id=entry["id"],
+                login=entry["login"],
+                display_name=entry["display_name"],
+                type=entry["type"],
+                broadcaster_type=entry["broadcaster_type"],
+                description=entry["description"],
+                profile_image_url=entry["profile_image_url"],
+                offline_image_url=entry["offline_image_url"],
+                view_count=entry["view_count"]
+            ) for entry in data["data"] if entry["login"] in login_names]
+
+    async def get_streams(self, users: List[TwitchUser]) -> List[TwitchStream]:
+        payload = {"user_id": [user.id for user in users]}
+
+        async with self.session.get("https://api.twitch.tv/helix/streams", params=payload,
+                                    headers=self.bearer_headers) as resp:
+            if resp.status != 200:
+                raise TwitchRequestError("Could not get streams", resp.status)
+
+            data = await resp.json()
+            return [
+                TwitchStream(
+                    id=entry["id"],
+                    user=discord.utils.get(users, id=entry["user_id"]),
+                    game_id=entry["game_id"],
+                    game_name=entry["game_name"],
+                    type=entry["type"],
+                    title=entry["title"],
+                    tags=entry["tags"],
+                    viewer_count=entry["viewer_count"],
+                    started_at=entry["started_at"],
+                    language=entry["language"],
+                    thumbnail_url=entry["thumbnail_url"]
+                ) for entry in data["data"]
+            ]
+
+    async def get_notifications(self) -> Optional[List[TwitchStream]]:
+        wl = config.get()["twitch"]["watchlist"]
+        users = await self.get_users(wl)
+        streams = await self.get_streams(users)
+
+        cache = []
+        for user_name in wl:
+            stream = discord.utils.get(streams, user__login=user_name)
+            if not stream:
+                try:
+                    self.online_users.remove(user_name)
+                except ValueError:
+                    pass
+                finally:
+                    continue
+
+            if user_name in self.online_users:
+                continue
+
+            cache.append(stream)
+            self.online_users.append(user_name)
+
+        return cache
+
+    @tasks.loop(minutes=2)
+    async def refresh_notify_check(self):
+        await self.bot.wait_until_ready()
+        channel = self.bot.get_channel(config.get()["twitch"]["channel_id"])
+        if not channel:
+            raise discord.DiscordException("Twitch Notification channel not found.")
+
+        streams = await self.get_notifications()
+        for stream in streams:
+            started_at = datetime.datetime.fromisoformat(stream.started_at).astimezone(datetime.timezone.utc)
+
+            embed = discord.Embed(title=stream.title, url=stream.user.url, color=random.randint(0, 0xFFFFFF))
+            embed.set_author(name=f"{stream.user.display_name} ist jetzt Live auf Twitch!", url=stream.user.url,
+                             icon_url=TWITCH_ICON_URL)
+            embed.set_thumbnail(url=stream.user.profile_image_url)
+            embed.add_field(name="Gestartet", value=discord.utils.format_dt(started_at, style="R"),
+                            inline=False)
+            embed.add_field(name="Spiel", value=stream.game_name or 'Unbekannt', inline=True)
+            embed.add_field(name="Viewers", value=f"{stream.viewer_count:,}", inline=True)
+            if tags := stream.tags:
+                embed.add_field(name="Tags", value=", ".join(tags), inline=False)
+            embed.set_image(url=stream.thumbnail_url.format(width=1920, height=1080))
+
+            try:
+                await channel.send(embed=embed)
+            except discord.HTTPException as exc:
+                logger.warning("Could not send twitch notification due to: %s", exc_info=True)
+
+
+async def setup(bot):
+    await bot.add_cog(TwitchNotifications(bot))
---
+++ youtube.py
@@ -0,0 +1,241 @@
+# -*- coding: utf-8 -*-
+
+"""
+MIT License
+
+Copyright (c) 2022-Present Klappstuhl
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+from __future__ import annotations
+import abc
+import logging
+import random
+from pathlib import Path
+from typing import List, Dict, Any, NamedTuple, Optional
+
+import aiohttp
+import discord
+import json
+
+from discord.ext import commands, tasks
+import datetime
+
+from discord.utils import cached_slot_property
+
+logger = logging.getLogger(__name__)
+
+
+class YouTubeRequestError(Exception):
+    def __init__(self, reason: str, status: int):
+        self.status = status
+        self.reason = reason
+        super().__init__(f"Request returned {status} thrown at: {reason}")
+
+
+class config(abc.ABCMeta):
+    """A class for getting and setting the config.json file.
+
+    Example JSON File
+    -----------------
+    {
+        "youtube": {
+            "api_key": "",
+            "channel_id": 0,
+            "watchlist": [
+                "PewDiePie"
+            ]
+        }
+    }
+
+    Example Folder Hierarchy
+    ------------------------
+    YourBotFolder <- Second "parent" folder
+        - cogs  <- First "parent" folder
+            - youtube.py  <- Current File
+        - main.py
+        - config.json
+    """
+
+    path = Path(__file__).parent.parent / "config.json"
+
+    @classmethod
+    def get(cls) -> Dict[str, Any]:
+        with open(cls.path, 'r', encoding='utf-8') as f:
+            return json.load(f)
+
+
+BASE_URL = "https://www.googleapis.com/youtube/v3/{endpoint}"
+YOUTUBE_ICON_URL = "https://media.discordapp.net/attachments/1062074624935993427/1101142491199180831/youtube-icon.png?width=519&height=519"
+YOUTUBE_VIDEO_URL = "https://www.youtube.com/watch?v={video_id}"
+
+
+class YouTubeChannel(NamedTuple):
+    id: str
+    name: str
+    icon_url: str
+
+    @property
+    def url(self) -> str:
+        return f"https://www.youtube.com/channel/{self.id}"
+
+
+class YouTubeStream(NamedTuple):
+    channel: YouTubeChannel
+    video_id: str
+    started_at: datetime.datetime
+    title: str
+    description: str
+    thumbnail_url: str
+
+    @property
+    def url(self) -> str:
+        return YOUTUBE_VIDEO_URL.format(video_id=self.video_id)
+
+
+class YouTubeNotifications(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot: commands.Bot = bot
+        self.session: aiohttp.ClientSession = aiohttp.ClientSession()
+
+        self.running_streams: List[YouTubeStream] = []
+
+    async def cog_load(self) -> None:
+        self.refresh_notify_check.start()
+
+    async def cog_unload(self) -> None:
+        if not self.session.closed:
+            await self.session.close()
+        self.refresh_notify_check.cancel()
+
+    @cached_slot_property(name="_cs_api_key")
+    def api_key(self) -> str:
+        return config.get()["youtube"].get("api_key", None)
+
+    @cached_slot_property(name="_cs_bearer_headers")
+    def bearer_headers(self) -> dict:
+        return {'Accept': 'application/json'}
+
+    def payload(self, **params: Any) -> Dict[str, Any]:
+        payload = {"key": self.api_key, **params}
+        return payload
+
+    async def get_channels(self, channel_names: List[str]) -> Optional[List[YouTubeChannel]]:
+        cache = []
+
+        for name in channel_names:
+            payload = self.payload(forUsername=name, part="id,snippet")
+            async with self.session.get(BASE_URL.format(endpoint="channels"), params=payload,
+                                        headers=self.bearer_headers) as resp:
+                if resp.status != 200:
+                    raise YouTubeRequestError(f'Could not get channel "{name}".', resp.status)
+
+                data = await resp.json()
+
+                if not data.get("items", None):
+                    continue
+
+                channel = data["items"][0]
+                cache.append(
+                    YouTubeChannel(
+                        id=channel["id"],
+                        name=channel["snippet"]["title"],
+                        icon_url=channel["snippet"]["thumbnails"]["default"]["url"]
+                    )
+                )
+
+        return cache
+
+    async def get_streams(self, channels: List[YouTubeChannel]):
+        cache = []
+
+        for channel in channels:
+            payload = self.payload(part="snippet", channelId=channel.id, type="video", eventType="live",
+                                   maxResults=1, order="date")
+            async with self.session.get(BASE_URL.format(endpoint="search"), params=payload,
+                                        headers=self.bearer_headers) as resp:
+                if resp.status != 200:
+                    raise YouTubeRequestError(f'Could not get stream for channel "{channel.id}".', resp.status)
+
+                data = await resp.json()
+
+                if not data.get("items", None):
+                    continue
+
+                stream = data["items"][0]
+                cache.append(
+                    YouTubeStream(
+                        channel=channel,
+                        video_id=stream["id"]["videoId"],
+                        started_at=datetime.datetime.fromisoformat(stream["snippet"]["publishedAt"])
+                        .astimezone(datetime.timezone.utc),
+                        title=stream["snippet"]["title"],
+                        description=stream["snippet"]["description"],
+                        thumbnail_url=stream["snippet"]["thumbnails"]["default"]["url"]
+                    )
+                )
+
+        return cache
+
+    async def get_notifications(self) -> List[YouTubeStream]:
+        wl = config.get()["youtube"]["watchlist"]
+        streams = await self.get_streams(await self.get_channels(wl))
+
+        cache = []
+        
+        for stream in self.running_streams:
+            if stream not in streams:
+                self.running_streams.remove(stream)
+        
+        for stream in streams:
+            if stream in self.running_streams:
+                continue
+
+            cache.append(stream)
+            self.running_streams.append(stream)
+
+        return cache
+
+    @tasks.loop(minutes=2)
+    async def refresh_notify_check(self):
+        await self.bot.wait_until_ready()
+        channel = self.bot.get_channel(config.get()["youtube"]["channel_id"])
+        if not channel:
+            raise discord.DiscordException("YouTube Notification channel not found.")
+
+        streams = await self.get_notifications()
+        for stream in streams:
+            embed = discord.Embed(title=stream.title,
+                                  description=stream.description,
+                                  url=stream.url, color=random.randint(0, 0xFFFFFF))
+            embed.set_author(name=f"{stream.channel.name} ist jetzt Live auf YouTube!", url=stream.channel.url,
+                             icon_url=YOUTUBE_ICON_URL)
+            embed.set_thumbnail(url=stream.channel.icon_url)
+            embed.add_field(name="Gestartet", value=discord.utils.format_dt(stream.started_at, style="R"),
+                            inline=False)
+            embed.set_image(url=stream.thumbnail_url)
+
+            try:
+                await channel.send(embed=embed)
+            except discord.HTTPException as exc:
+                logger.warning("Could not send twitch notification due to: %s", exc_info=True)
+
+
+async def setup(bot):
+    await bot.add_cog(YouTubeNotifications(bot))
